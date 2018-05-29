b8fccac85df2a66f2dc845c28cabf33adad1f9f1
Kevin <stradtkt22@gmail.com>
2018-05-28 21:17:21 -0400

    Timezones initial commit

---
+++ index.py
@@ -0,0 +1,19 @@
+import pytz
+import datetime
+
+country = "US/Eastern"
+tz_to_display = pytz.timezone(country)
+local_time = datetime.datetime.now(tz=tz_to_display)
+print("The time in {} is {}".format(country, local_time))
+print("UTC is {}".format(datetime.datetime.utcnow()))
+
+
+for x in sorted(pytz.country_names):
+  print("{}: {}".format(x, pytz.country_names[x]), end=': ')
+  if x in pytz.country_timezones:
+    for zone in sorted(pytz.country_timezones[x]):
+      tz_to_display = pytz.timezone(zone)
+      local_time = datetime.datetime.now(tz=tz_to_display)
+    print("\t\t{}: {}".format(zone, local_time))
+  else:
+    print("No time zone defined")
